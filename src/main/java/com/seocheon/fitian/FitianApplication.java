2c27b0d1df2ad3dbf68e22035510c44a3f6810cc
kwanjung <kwanjungkim@naver.com>
2024-10-14 17:34:52 +0900

    reset

---
+++ src/main/java/com/seocheon/fitian/FitianApplication.java
@@ -0,0 +1,12 @@
+package com.seocheon.fitian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitianApplication.class, args);
+	}
+}
