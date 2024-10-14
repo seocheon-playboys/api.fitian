a646a0b38495b62a35407691df87d2ec789d7937
kwanjung <kwanjungkim@naver.com>
2024-10-14 16:25:21 +0900

    first commit

---
+++ src/main/java/com/seocheon/fitian/FitianApplication.java
@@ -0,0 +1,13 @@
+package com.seocheon.fitian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seocheon/fitian/FitianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seocheon.fitian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
