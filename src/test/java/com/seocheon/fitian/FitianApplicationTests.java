8cefa738ad50fc07f82840d50d717323baec683c
kwanjung <kwanjungkim@naver.com>
2024-11-13 17:05:35 +0900

    delete @SpringbootTest annotation

--- src/test/java/com/seocheon/fitian/FitianApplicationTests.java
+++ src/test/java/com/seocheon/fitian/FitianApplicationTests.java
@@ -1,9 +1,8 @@
 package com.seocheon.fitian;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FitianApplicationTests {
 
 	@Test
