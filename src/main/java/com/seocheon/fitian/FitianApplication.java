d4f2032862e8b46a022b9981fc2272eb1a8472f5
kwanjung <kwanjungkim@naver.com>
2024-11-13 17:23:26 +0900

    modify fitianapplication

--- src/main/java/com/seocheon/fitian/FitianApplication.java
+++ src/main/java/com/seocheon/fitian/FitianApplication.java
@@ -1,9 +1,11 @@
 package com.seocheon.fitian;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.seocheon.fitian.mapper")
 public class FitianApplication {
 
 	public static void main(String[] args) {
