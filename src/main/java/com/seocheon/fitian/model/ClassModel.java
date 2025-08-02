9d43482ca843de3f64d40c3d027f5c6c2390e790
kwanjung <kwanjungkim@naver.com>
2025-08-02 19:07:36 +0900

    fix : localDate 타입의 스트링 널 체크 제거

--- src/main/java/com/seocheon/fitian/model/ClassModel.java
+++ src/main/java/com/seocheon/fitian/model/ClassModel.java
@@ -2,13 +2,17 @@ package com.seocheon.fitian.model;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassModel {
 
 	private int classNo;
