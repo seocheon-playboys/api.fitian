bc6153a5a1ab1cb4f43779da50d610b752d4f092
kwanjung <kwanjungkim@naver.com>
2025-08-25 14:12:50 +0900

    fix : 날짜 직렬화

--- src/main/java/com/seocheon/fitian/dto/classes/SearchClassDTO.java
+++ src/main/java/com/seocheon/fitian/dto/classes/SearchClassDTO.java
@@ -2,15 +2,23 @@ package com.seocheon.fitian.dto.classes;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchClassDTO {
 	private String boxCode;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate startDate;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate endDate;
 }
